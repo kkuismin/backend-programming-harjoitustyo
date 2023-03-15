f4b623b6eccdbf5e6b568fe1a163cc42fcdddcc7
kkuismin <katariina.kuismin@gmail.com>
2023-03-15 17:53:37 +0200

    Adding edit and delete functionality

--- src/main/java/harjoitustyo/FoodTracker/web/FoodController.java
+++ src/main/java/harjoitustyo/FoodTracker/web/FoodController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -46,6 +47,20 @@ public class FoodController {
 	@PostMapping("/saveFood")
 	public String saveFood(Food food, Model model) {
 		foodrepository.save(food);
+		return "redirect:/foodlist";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String editFood(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("food", foodrepository.findById(id));
+		model.addAttribute("locations", locationrepository.findAll());
+		model.addAttribute("foodTypes", typerepository.findAll());
+		return "editfood";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteFood(@PathVariable("id") Long id, Model model) {
+		foodrepository.deleteById(id);
 		return "redirect:/foodlist";
 	}
 }
