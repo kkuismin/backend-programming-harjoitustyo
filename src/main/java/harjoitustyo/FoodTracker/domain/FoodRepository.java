8e3b45baa56e697d2eba33966fce29cdcea0e77f
kkuismin <katariina.kuismin@gmail.com>
2023-03-15 19:25:41 +0200

    Editing frontend

--- src/main/java/harjoitustyo/FoodTracker/domain/FoodRepository.java
+++ src/main/java/harjoitustyo/FoodTracker/domain/FoodRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface FoodRepository extends CrudRepository<Food, Long> {
 	List<Food> findByName(String name);
-	
-	List<Food> findById(String id);
 }
